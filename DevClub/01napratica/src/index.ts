c7b8d41c8468d9b546eb003419e5081ad34f80af
GersonESS <gebhsantos@gmail.com>
2025-01-26 06:02:43 -0300

    Update index.ts

--- DevClub/01napratica/src/index.ts
+++ DevClub/01napratica/src/index.ts
@@ -2,7 +2,37 @@ import express, { Request, Response} from 'express';
 import cors from 'cors';
 
 const app = express();
+//app.use(express.json());
 app.use(cors());
+enum Estados {
+    MG = 'Minas Gerais',
+    SP = 'São Paulo',
+    RJ = 'Rio de Janeiro',
+    ES = 'Espírito Santo',
+    PR = 'Paraná',
+    SC = 'Santa Catarina',
+    RS = 'Rio Grande do Sul',
+    MS = 'Mato Grosso do Sul',
+    MT = 'Mato Grosso',
+    GO = 'Goiás',
+    DF = 'Distrito Federal',
+    TO = 'Tocantins',
+    PA = 'Pará',
+    AP = 'Amapá',
+    RO = 'Rondônia',
+    AC = 'Acre',
+    AM = 'Amazonas',
+    RR = 'Roraima',
+    CE = 'Ceará',
+    RN = 'Rio Grande do Norte',
+    PB = 'Paraíba',
+    PE = 'Pernambuco',
+    AL = 'Alagoas',
+    SE = 'Sergipe',
+    BA = 'Bahia',
+    PI = 'Piauí',
+    MA = 'Maranhão'
+}
 interface IProdutos
      {
     id: number;
@@ -12,6 +42,8 @@ interface IProdutos
         rua: string;
         numero: number;
         bairro: string;
+        cidade: string;
+        estado?: Estados;
              }
     }
 let produtos: IProdutos[] = []
@@ -26,12 +58,14 @@ app.get('/produtos', (req: Request, res: Response) => {
                 {
                 rua: 'Arceburgo',
                 numero: 20,
-                bairro: 'Bomfim'
+                bairro: 'Bomfim',
+                cidade: 'Belo Horizonte',
+                estado: Estados.MG
                 }
               }
               );
               produtos.push (newProdutos);
-              return res.send(produtos);
+              return res.json(produtos);
         });
 
 app.listen(8080, () => 
