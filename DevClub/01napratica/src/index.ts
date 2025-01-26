12920cd27927419f1cb38b727f1d4d99228769f4
GersonESS <gebhsantos@gmail.com>
2025-01-25 23:10:55 -0300

    a

--- DevClub/01napratica/src/index.ts
+++ DevClub/01napratica/src/index.ts
@@ -1,30 +1,40 @@
 import express, { Request, Response} from 'express';
-const app = express();
+import cors from 'cors';
 
-interface IProdutos {
+const app = express();
+app.use(cors());
+interface IProdutos
+     {
+    id: number;
     nome: string;
     preco: number;
     endereco: {
         rua: string;
         numero: number;
+        bairro: string;
+             }
     }
-}
 let produtos: IProdutos[] = []
 
-app.get('/produtos', (req, res) => {
+app.get('/produtos', (req: Request, res: Response) => {
 
-  produtos.push({
-                nome: 'produto1', 
-                preco: 10,
-                endereco:
+  const newProdutos=({
+        id: Math.random(),
+        nome: 'Batedeira de Bolo walita', 
+        preco: 10,
+        endereco:
                 {
-                    rua: 'rua1',
-                    numero: 1
+                rua: 'Arceburgo',
+                numero: 20,
+                bairro: 'Bomfim'
                 }
               }
-    //res.send('meus produtos!');
-  });
+              );
+              produtos.push (newProdutos);
+              return res.send(produtos);
+        });
 
-app.listen(8080, () => {
+app.listen(8080, () => 
+    {
     console.log('Rodando app listening at http://localhost:8080');
-  });
+   } );
