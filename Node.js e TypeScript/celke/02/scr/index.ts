16977f609cc2f69f81889970741132c548dff45e
GersonESS <gebhsantos@gmail.com>
2025-02-13 15:32:06 -0300

    a

--- Node.js e TypeScript/celke/02/scr/index.ts
+++ Node.js e TypeScript/celke/02/scr/index.ts
@@ -1,9 +1,21 @@
 // Importar a biblioteca Express
 import express, {Request, Response} from "express";
 
+// Importar o arquivo com as credenciais do banco de dados
+import { AppDataSource } from "./data-source";
+
 // Criar a aplicação Express
 const app = express();
 
+// iniciar a conexão com o banco de dados
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Conexão com o banco de dados realizada com sucesso!");
+    }).catch((error) => {
+        console.log("Erro ao conectar com o banco de dados: " , error);
+    });
+
+    
 // Criar a rota GET principal
 app.get("/", (req: Request, res: Response) => {
     res.send("Bem-vindo Celke!");
