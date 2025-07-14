884673087893bfbef4823a3f2259166c3ed94385
GersonESS <gebhsantos@gmail.com>
2025-07-14 18:03:07 -0300

    h

---
+++ DevClub/04/backend/src/index.ts
@@ -0,0 +1,79 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+enum States{
+    AC = 'Acre',
+    AL = 'Alagoas',
+    AP = 'Amapá',
+    AM = 'Amazonas',
+    BA = 'Bahia',
+    CE = 'Ceará',
+    DF = 'Distrito Federal',
+    ES = 'Espírito Santo',
+    GO = 'Goiás',       
+    MA = 'Maranhão',
+    MT = 'Mato Grosso',
+    MS = 'Mato Grosso do Sul',
+    MG = 'Minas Gerais',
+    PA = 'Pará',
+    PB = 'Paraíba',         
+    PR = 'Paraná',
+    PE = 'Pernambuco',
+    PI = 'Piauí',
+    RJ = 'Rio de Janeiro',
+    RN = 'Rio Grande do Norte',
+    RS = 'Rio Grande do Sul',
+    RO = 'Rondônia',
+    RR = 'Roraima',
+    SC = 'Santa Catarina',
+    SP = 'São Paulo',
+    SE = 'Sergipe',
+    TO = 'Tocantins'
+    }
+
+interface IProducts {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    address: IAddress;
+
+}
+
+interface IAddress {
+    street: string;
+    number: number;
+    city: string;
+    state: string;
+    }
+
+let produts: IProducts[] = [];
+
+
+app.get('/', function (req, res) {
+    res.send('API is running');
+});
+
+app.get('/produtos', (req: Request, res: Response) => {
+    const newProduct: IProducts = {
+        id: Math.random(),
+        name: 'Produto ',
+        price: 100,
+        quantity: 10,
+        address: {
+            street: 'Rua A',
+            number: 123,
+            city: 'Cidade B',
+            state: States.AC
+        }
+    }
+
+    produts.push(newProduct);
+    return res.json(produts);
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
