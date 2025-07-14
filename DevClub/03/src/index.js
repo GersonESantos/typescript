426c05abd8bfef7187c59aabf9cbf9e77e944c0a
GersonESS <gebhsantos@gmail.com>
2025-07-14 05:55:26 -0300

    a

---
+++ DevClub/03/src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// filepath: src/index.ts
+var express = require("express");
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
+});
+app.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
