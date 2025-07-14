e2e742258e3d8a7813e85cef2f257049b42803dc
GersonESS <gebhsantos@gmail.com>
2025-07-14 17:25:44 -0300

    Update index.ts

--- DevClub/02/backend/src/index.ts
+++ DevClub/02/backend/src/index.ts
@@ -1,6 +1,19 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+interface IProducts {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    address: {
+        street: string;
+        number: number;
+        city: string;
+        state: string;
+    }
+}
+let produts: IProducts[] = [];
 
 
 app.get('/', function (req, res) {
@@ -8,8 +21,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/produtos', (req: Request, res: Response) => {
-    return res.send('Ja temos: produtos');
+    const newProduct: IProducts = {
+        id: Math.random(),
+        name: 'Produto ',
+        price: 100,
+        quantity: 10,
+        address:{
+            street: 'Rua A',
+            number: 123,
+            city: 'Cidade B',
+            state: 'Estado C' 
+        }
+    }
+
+    produts.push(newProduct);
+    return res.json(produts);
 });
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
